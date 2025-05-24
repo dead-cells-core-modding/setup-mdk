327eeee1cee40fe8588e9d369335c5ce0316ebc6
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2025-05-24 21:45:03 +0800

    feat: first

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { existsSync, mkdirSync, readFileSync } from 'fs';
+import path from 'path';
+import { unzip } from './unzip.js'
+import { execSync } from 'child_process';
 
 /**
  * The main function for the action.
@@ -8,18 +11,58 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    let mcr = core.getInput('modcoreroot');
+    let url : string | null = core.getInput('downloadurl');
+    if(url == '' || !url) {
+      url = 'https://github.com/dead-cells-core-modding/core/releases/latest/download/win-x64.zip';
+    }
+    if(mcr == '' || !mcr) {
+      mcr = path.join(core.getInput("GITHUB_WORKSPACE"), '.dccm-mdk');
+      mkdirSync(mcr, {
+        recursive: true
+      });
+    } else {
+      url = null;
+    }
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    let modcoreRoot = path.resolve(mcr);
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`ModCoreRoot: ${modcoreRoot}`);
+    core.debug(`Download url: ${url}`);
 
+    if(url) {
+      core.info(`Downloading dccm from ${url}`);
+      const resp = await fetch(url);
+      const data = await resp.arrayBuffer();
+      core.debug(`Resp ${resp.statusText}: ${data.byteLength}`);
+      core.info(`Extracting DCCM`);
+      await unzip(Buffer.from(data), modcoreRoot);
+      if(existsSync(path.join(modcoreRoot, 'win-x64', 'ModCoreVersion.txt'))) {
+        modcoreRoot = path.join(modcoreRoot, 'win-x64');
+      }
+    }
+
+    const mdkRoot = path.resolve(modcoreRoot, "core", "mdk");
+
+    const modcoreVersionTxt = path.join(modcoreRoot, 'ModCoreVersion.txt');
+    core.debug(`ModCoreVersion.txt path: ${modcoreVersionTxt}`);
+    if(!existsSync(modcoreVersionTxt)) {
+      core.setFailed('Unable to find `ModCoreVersion.txt`, MDK may be corrupted.');
+      return;
+    }
+    const dccmVer = readFileSync(modcoreVersionTxt, 'utf-8');
+    core.info(`Dead Cells Core Modding Version: ${dccmVer}`);
+
+    core.info(`Installing`);
+    const installScript = path.join(mdkRoot, 'install.ps1');
+    execSync(`pwsh -File "${installScript}"`);
+
+    core.info('DCCM MDK is installed');
+    
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.exportVariable("DCCM_MDK_ROOT", mdkRoot);
+    core.setOutput('mdkroot', mdkRoot);
+    core.setOutput('modcoreroot', modcoreRoot);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
---
+++ src/unzip.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import JSZip from 'jszip';
+
+
+export async function unzip(zipData: Buffer, destPath: string) {
+    const zip = await JSZip.loadAsync(zipData);
+
+    for (const relativePath of Object.keys(zip.files)) {
+        const file = zip.files[relativePath];
+        const filePath = path.join(destPath, relativePath);
+
+        if (file.dir) {
+            fs.mkdirSync(filePath, { recursive: true });
+        } else {
+            const content = await file.async('nodebuffer');
+            const dir = path.dirname(filePath);
+            fs.mkdirSync(dir, {
+                recursive: true
+            });
+            fs.writeFileSync(filePath, content);
+        }
+    }
+}
